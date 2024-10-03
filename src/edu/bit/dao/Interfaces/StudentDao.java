8a2196fced86757220d19336a3dea949c55c96a5
Administrator <Administrator@DESKTOP-GARLJAI>
2024-10-03 10:41:53 -0700

    Defines the methods for accessing student data in the database.

---
+++ src/edu/bit/dao/Interfaces/StudentDao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.bit.dao.Interfaces;
+
+import edu.bit.dao.CrudDao;
+import edu.bit.entity.StudentEntity;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface StudentDao extends CrudDao<StudentEntity, String>{
+    
+}
