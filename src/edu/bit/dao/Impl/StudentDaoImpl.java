19a2077966a8aa64315df0663cf08b862a7b0ed3
Administrator <Administrator@DESKTOP-GARLJAI>
2024-10-03 10:40:57 -0700

    Implements the CRUD operations defined in StudentDao for student-related database interactions.

---
+++ src/edu/bit/dao/Impl/StudentDaoImpl.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.bit.dao.Impl;
+
+import edu.bit.dao.CrudUtil;
+import edu.bit.dao.Interfaces.StudentDao;
+import edu.bit.entity.StudentEntity;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class StudentDaoImpl implements StudentDao{
+
+    @Override
+    public boolean save(StudentEntity t) throws Exception {
+        return CrudUtil.executeUpdate("INSERT INTO student VALUES (?,?,?,?,?,?,?)",t.getStudent_Id(),t.getName(),t.getAddress(),t.getBirth(),t.getFmName(),t.getContact(),t.getGender());
+    }
+
+    @Override
+    public boolean update(StudentEntity t) throws Exception {
+        return CrudUtil.executeUpdate("UPDATE student SET Student_Name=?,Address=?, Date_of_Birth=?, Father_Mother_Name=?, Contact_Name=?, Gender=? WHERE Student_ID=?", t.getName(),t.getAddress(),t.getBirth(),t.getFmName(),t.getContact(),t.getGender(),t.getStudent_Id());
+    }
+
+    @Override
+    public boolean dalete(String Id) throws Exception {
+        return CrudUtil.executeUpdate("DELETE FROM student WHERE Student_ID=?", Id);
+    }
+
+    @Override
+    public StudentEntity get(String Id) throws Exception {
+        ResultSet rst=CrudUtil.executeQuery("SELECT * FROM student WHERE Student_ID=?", Id);
+        
+        if (rst.next()) {
+            StudentEntity studentEntity=new StudentEntity(rst.getString("student_Id"), rst.getString("name"), rst.getString("address"), rst.getDate("birth"), rst.getString("fmName"), rst.getString("contact"), rst.getString("gender"));
+            return studentEntity;
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<StudentEntity> getAll() throws Exception {
+        ArrayList<StudentEntity> studentEntities=new ArrayList<>();
+        ResultSet rst=CrudUtil.executeQuery("SELECT * FROM student");
+        while (rst.next()) {
+            StudentEntity studentEntity=new StudentEntity(rst.getString("student_Id"), rst.getString("name"), rst.getString("address"), rst.getDate("birth"), rst.getString("fmName"), rst.getString("contact"), rst.getString("gender"));
+            
+            studentEntities.add(studentEntity);
+        }
+        return studentEntities;
+    }
+    
+}
